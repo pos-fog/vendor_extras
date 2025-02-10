1859ee0bde377c3ec1963a8b97ab98fa071c4bed
darkphnx <dark.phnx12@gmail.com>
2025-02-10 18:14:59 +0530

    extras: Add GeneralSans font overlay
    taken from https://github.com/DroidX-UI/vendor_droidx/tree/15/fonts
    
    Co-authored-by: ZirgomHaidar <zirgomhaidar17@gmail.com>

--- evolution.mk
+++ evolution.mk
@@ -132,6 +132,7 @@ PRODUCT_PACKAGES += \
     FontEvolveSansOverlay \
     FontFifa2018Overlay \
     FontGrandHotelOverlay \
+    FontGeneralSansOverlay \
     FontHarmonySansOverlay \
     FontLatoOverlay \
     FontLinotteOverlay \
