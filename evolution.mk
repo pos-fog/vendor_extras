afceac4e7ca61f8bb952ac98b96f22655b293266
Pranav Vashi <neobuddy89@gmail.com>
2025-03-24 12:30:27 +0900

    extras: Remove gestural navigation overlays
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- evolution.mk
+++ evolution.mk
@@ -380,11 +380,6 @@ PRODUCT_PACKAGES += \
     BrightnessSliderThumbSliderOverlay \
     BrightnessSliderTranslucentOverlay
 
-# Navbar
-PRODUCT_PACKAGES += \
-    LineageNavigationBarNarrowSpace \
-    LineageNavigationBarNoSpace
-
 # Navbar styles
 PRODUCT_PACKAGES += \
     NavbarAndroidOverlay \
