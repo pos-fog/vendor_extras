3905544fe0f65092fe1156cd857befd4770f970e
alternoegraha <noegrahachan@gmail.com>
2025-04-25 08:47:03 +0700

    extras: replace Accord with Gramophone
    - APK version 10.16.1 (https://github.com/AkaneTan/Gramophone/releases/tag/1.0.16.1)
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- extras.mk
+++ extras.mk
@@ -8,10 +8,10 @@
 CUSTOM_EXTRAS_PATH := vendor/extras
 
 # Accord
-TARGET_INCLUDE_ACCORD ?= true
-ifeq ($(TARGET_INCLUDE_ACCORD),true)
+TARGET_INCLUDE_GRAMOPHONE ?= true
+ifeq ($(TARGET_INCLUDE_GRAMOPHONE),true)
 PRODUCT_PACKAGES += \
-    Accord
+    Gramophone
 endif
 
 # GrapheneCamera
